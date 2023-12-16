913ab16b19d79ba1c7b340a05c9e0ea9a9b6be46
pvlkurial <pavelkurial0@gmail.com>
2023-12-16 15:16:40 +0100

    Whole code in c

---
+++ AOC23-01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int getNumberOfLine(char * array, size_t lenght){
+    int dig1;
+    bool isInit = false;
+    int dig2;
+    for(size_t i = 0; i < lenght ; i++){
+        if (isdigit(array[i])){
+            if(!isInit){
+                dig1 = array[i]-'0';
+                isInit = true;
+            }
+            dig2 = array[i]-'0';
+
+        }        
+    }
+    free(array);
+    printf("%d, %d\n", dig1, dig2);
+    return 10*dig1+dig2;
+}
+
+int main(){
+int sum = 0;
+
+while(!feof(stdin)){
+    char * array;
+    size_t bufsize = 0;
+    size_t numOfChars = getline(&array, &bufsize, stdin);
+    if(numOfChars==NULL){
+        break;
+    }
+    sum += getNumberOfLine(array, numOfChars);
+
+}
+printf("%d\n", sum);
+return EXIT_SUCCESS;
+}
